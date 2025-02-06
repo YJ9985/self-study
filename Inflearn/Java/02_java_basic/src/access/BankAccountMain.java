51175e9107cdc064b6a7d24fe2f1b08386f4fe4a
Yeji <lucy1486@naver.com>
2025-02-07 00:18:12 +0900

    학습

--- Inflearn/Java/02_java_basic/src/access/BankAccountMain.java
+++ Inflearn/Java/02_java_basic/src/access/BankAccountMain.java
@@ -8,3 +8,4 @@ public class BankAccountMain {
         System.out.println("balance = " + account.getBalance());
     }
 }
+
---
+++ Inflearn/Java/02_java_basic/src/poly/car/Model3Car.java
@@ -0,0 +1,16 @@
+package poly.car;
+
+public class Model3Car {
+
+    public void startEngine() {
+        System.out.println("Model3Car Engine started");
+    }
+
+    public void offEngine() {
+        System.out.println("Model3Car.offEngine");
+    }
+
+    public void pressAccelerator() {
+        System.out.println("Model3Car.pressAccelerator");
+    }
+}
