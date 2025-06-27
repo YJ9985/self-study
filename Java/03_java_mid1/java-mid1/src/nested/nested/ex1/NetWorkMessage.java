bf5d45210dd383cbe386d26b84ec2e2ef4242e0f
YJ9985 <lucy1486@naver.com>
2025-06-27 16:32:07 +0900

    250627

---
+++ Java/03_java_mid1/java-mid1/src/nested/nested/ex1/NetWorkMessage.java
@@ -0,0 +1,14 @@
+package nested.nested.ex1;
+
+public class NetWorkMessage {
+
+    private String content;
+
+    public NetWorkMessage(String content) {
+        this.content = content;
+    }
+
+    public void print() {
+        System.out.println(content);
+    }
+}
